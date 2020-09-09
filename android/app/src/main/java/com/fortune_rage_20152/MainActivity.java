a8f40349630d0c70bbc5ef7bde28036f2074831a
Crowdbotics <team@crowdbotics.com>
2020-09-09 20:35:19 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/fortune_rage_20152/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fortune_rage_20152;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "fortune_rage_20152";
+    }
+}
